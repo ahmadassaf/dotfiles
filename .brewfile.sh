55a295e496a79e9f89b2680ffdb2999e413cd9e1
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2014-11-06 19:40:07 +0100

    imporving brew and caskfile

--- .brewfile.sh
+++ .brewfile.sh
@@ -1,86 +1,80 @@
-# Install command-line tools using Homebrew
-# Usage: `brew bundle Brewfile`
+# Install Homebrew.
+if [[ ! "$(type -P brew)" ]]; then
+  echo "Installing Homebrew"
+  true | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-# Make sure we’re using the latest Homebrew
+echo "Updating Homebrew"
+brew doctor
 brew update
 
-# Upgrade any already-installed formulae
-brew upgrade
+# Install Homebrew recipes.
+function brew_install_recipes() {
+  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
+  if (( ${#recipes[@]} > 0 )); then
+    echo "Installing Homebrew recipes: ${recipes[*]}"
+    for recipe in "${recipes[@]}"; do
+      brew install $recipe
+    done
+  fi
+}
 
-# Install GNU core utilities (those that come with OS X are outdated)
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-#sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# Homebrew recipes
+recipes=(
+	coreutils
+	moreutils
+	findutils
+	gnu-sed --default-names
+  bash
+  bash-completion
+  cowsay
+  git
+  git-extras
+  htop-osx
+  hub
+  id3tool
+  lesspipe
+  man2html
+  mercurial
+  sl
+  ssh-copy-id
+  the_silver_searcher
+  tree
+  wget --enable-iri
+  ringojs
+	narwhal
+	vim --override-system-vi
+	homebrew/dupes/grep
+	homebrew/dupes/screen
+	bfg
+	binutils
+	binwalk
+	cifer
+	dex2jar
+	dns2tcp
+	fcrackzip
+	foremost
+	hashpump
+	hydra
+	john
+	knock
+	pngcheck
+	sqlmap
+	xz
+	ack
+	lynx
+	node
+	p7zip
+	pigz
+	pv
+	rename
+	rhino
+	webkit2png
+	zopfli
+	heroku
+)
 
-# Install some other useful utilities like `sponge`
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`
-brew install gnu-sed --default-names
-# Install Bash 4
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before running `chsh`.
-brew install bash
-brew install bash-completion
-
-# Install wget with IRI support
-brew install wget --enable-iri
-
-# Install RingoJS and Narwhal
-# Note that the order in which these are installed is important; see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
-
-#install cask
-brew tap phinze/cask
-brew brew install brew-cask
-
-# Install more recent versions of some OS X tools
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install nmap
-brew install pngcheck
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` et al.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries
-brew brew install ack
-#install exiv2
-brew install git
-brew install imagemagick --with-webp
-brew install lynx
-brew install node # This installs `npm` too using the recommended installation method
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install tree
-brew install webkit2png
-brew install zopfli
-
-brew install homebrew/versions/lua52
-
+# Start the actual installation of the recipes
+brew_install_recipes
 # Remove outdated versions from the cellar
 brew cleanup
--- .caskfile.sh
+++ .caskfile.sh
@@ -1,20 +1,92 @@
-# Install native apps
-# Usage: `brew bundle Caskfile`
+# Exit if Homebrew is not installed.
+[[ ! "$(type -P brew)" ]] && e_error "Brew casks need Homebrew to install." && return 1
 
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
+# Tap Homebrew kegs.
+function brew_tap_kegs() {
+  kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
+  if (( ${#kegs[@]} > 0 )); then
+    echo "Tapping Homebrew kegs: ${kegs[*]}"
+    for keg in "${kegs[@]}"; do
+      brew tap $keg
+    done
+  fi
+}
 
-brew cask install dropbox 2> /dev/null
-brew cask install google-chrome 2> /dev/null
-brew cask install google-chrome-canary 2> /dev/null
-brew cask install imagealpha 2> /dev/null
-brew cask install imageoptim 2> /dev/null
-brew cask install iterm2 2> /dev/null
-brew cask install macvim 2> /dev/null
-brew cask install sublime-text 3> /dev/null
-brew cask install the-unarchiver 2> /dev/null
-brew cask install torbrowser 2> /dev/null
-brew cask install transmission 2> /dev/null
-brew cask install ukelele 2> /dev/null
-brew cask install virtualbox 2> /dev/null
-brew cask install vlc 2> /dev/null
+# Ensure the cask keg and recipe are installed.
+kegs=(caskroom/cask)
+brew_tap_kegs
+
+# Exit if, for some reason, cask is not installed.
+[[ ! "$(brew ls --versions brew-cask)" ]] && e_error "Brew-cask failed to install." && return 1
+
+# Hack to show the first-run brew-cask password prompt immediately.
+brew cask info this-is-somewhat-annoying 2>/dev/null
+
+# Homebrew casks
+casks=(
+  # Applications
+  a-better-finder-rename
+  bettertouchtool
+  charles
+  dropbox
+  fastscripts
+  firefox
+  google-chrome
+  gyazo
+  hex-fiend
+  iterm2
+  launchbar
+  macvim
+  spotify
+  steam
+  the-unarchiver
+  todoist
+  tower
+  transmission-remote-gui
+  vlc
+  sublime-text-3
+  imagealpha
+	imageoptim
+  # Quick Look plugins
+  betterzipql
+  qlcolorcode
+  qlmarkdown
+  qlprettypatch
+  qlstephen
+  quicklook-csv
+  quicklook-json
+  quicknfo
+  suspicious-package
+  webp-quicklook
+  # Color pickers
+  colorpicker-developer
+  colorpicker-skalacolor
+)
+
+# Install Homebrew casks.
+casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
+if (( ${#casks[@]} > 0 )); then
+  echo "Installing Homebrew casks: ${casks[*]}"
+  for cask in "${casks[@]}"; do
+    brew cask install $cask
+  done
+  brew cask cleanup
+fi
+
+# Work around colorPicker symlink issue.
+# https://github.com/caskroom/homebrew-cask/issues/7004
+cps=()
+for f in ~/Library/ColorPickers/*.colorPicker; do
+  [[ -L "$f" ]] && cps=("${cps[@]}" "$f")
+done
+
+if (( ${#cps[@]} > 0 )); then
+  echo "Fixing colorPicker symlinks"
+  for f in "${cps[@]}"; do
+    target="$(readlink "$f")"
+    echo "$(basename "$f")"
+    rm "$f"
+    cp -R "$target" ~/Library/ColorPickers/
+  done
+fi
+
