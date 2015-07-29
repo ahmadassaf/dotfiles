f7f2bdb38329571904ce97ee1cc3a0f154342059
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-07-29 15:24:04 +0200

    adding file to install npm globals

---
+++ .npm_globals.sh
@@ -0,0 +1,32 @@
+# Install NPM globals
+function npm_install_globals() {
+  for global in "${globals[@]}"; do
+    sudo npm install -g $global
+  done
+}
+
+globals=(
+  amok
+  bower
+  caniuse
+  cli
+  eslint
+  grunt
+  imageoptim
+  jscs
+  mocha
+  mongoose
+  node
+  nodemon
+  npm
+  prettyjson
+  psi
+  should
+  slap
+  sparkly
+  tmi
+  vtop
+)
+
+# Start the actual installation of the recipes
+npm_install_globals
