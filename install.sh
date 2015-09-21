0f30fe6b81523354f93d9df3060a50bc115b72a8
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-09-21 19:55:02 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,13 +16,13 @@ function linkDotFiles() {
 	find $SOURCE_LOCATION -mindepth 1 -maxdepth 1 -name '.*'| while read file; do
 		if [[ $(basename $file) != ".git" && $(basename $file) != ".gitignore" ]]; then
   		if [[ -d ${HOME}/$(basename $file) ]]; then
-  			echo "${NC}$(basename $file)${NC} is a driectory, ${red}removing a directory now ..${NC}"
+  			printf "${NC}$(basename $file)${NC} is a driectory, ${red}removing a directory now ..${NC}"
   			rm -r "${HOME}/$(basename $file)"
   		else
-  			echo "${magenta}$(basename $file)${NC} is found, ${red}deleting and overwriting now!${NC}"
+  			printf "${magenta}$(basename $file)${NC} is found, ${red}deleting and overwriting now!${NC}"
   			rm "${HOME}/$(basename $file)"
 			fi
-		  echo "Linking ${magenta}$file${NC} to ${magenta}$TARGET_LOCATION/$(basename $file)${NC}"
+		  printf "Linking ${magenta}$file${NC} to ${magenta}$TARGET_LOCATION/$(basename $file)${NC}"
 	    ln -s "$file" "${HOME}/$(basename $file)"
 		fi
 	done
@@ -41,9 +41,9 @@ unset linkDotFiles;
 
 if [[ ! -e ${HOME}/.dircolors ]]; then
 	# Prompt the user if he wishes to have dircolors enabled
-	echo "${magenta}\nDircolors${NC} helps applying different colors for file groups when executing ${red}ls${NC}\n"
+	printf "${magenta}\nDircolors${NC} helps applying different colors for file groups when executing ${red}ls${NC}\n"
 	read -p "This will enable dircolors. Are you sure? [Y/N]" -n 1;
-	echo "";
+	printf "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		ln -s "$SOURCE_LOCATION/dotfiles/dircolors/LS_COLORS" "${HOME}/.dircolors"
 	fi;
