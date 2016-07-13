32e49fcd239bec24d03af4313ae073d3de06f94b
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-07-13 12:44:07 +0100

    merging upstream changes

--- install.sh
+++ install.sh
@@ -12,20 +12,21 @@ export SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SOURCE_LOCATION="$SCRIPT_DIRECTORY" # Contains the files and directories I want to work with.
 
 function linkDotFiles() {
-	# Link the files from source to the target with a dot appended to the front.
-	find $SOURCE_LOCATION -mindepth 1 -maxdepth 1 -name '.*'| while read file; do
-		if [[ $(basename $file) != ".git" && $(basename $file) != ".gitignore" ]]; then
-  		if [[ -d ${HOME}/$(basename $file) ]]; then
-  			printf "${NC}$(basename $file)${NC} is a driectory, ${red}removing a directory now ..\n${NC}"
-  			rm -r "${HOME}/$(basename $file)"
-  		else
-  			printf "${magenta}$(basename $file)${NC} is found, ${red}deleting and overwriting now!\n${NC}"
-  			rm "${HOME}/$(basename $file)"
-			fi
-		  printf "Linking ${magenta}$file${NC} to ${magenta}$TARGET_LOCATION/$(basename $file)\n${NC}"
-	    ln -s "$file" "${HOME}/$(basename $file)"
-		fi
-	done
+    # Link the files from source to the target with a dot appended to the front.
+    find $SOURCE_LOCATION/dotfiles -mindepth 1 -maxdepth 1 -name '.*'| while read file; do
+        if [[ $(basename $file) != ".git" && $(basename $file) != ".gitignore" ]]; then
+            if [[ -d ${HOME}/$(basename $file) ]]; then
+                echo "${NC}$(basename $file)${NC} is a driectory, ${red}removing a directory now ..${NC}"
+                rm -r "${HOME}/$(basename $file)"
+            elif [[ -f ${HOME}/$(basename $file) ]]
+                echo "${magenta}$(basename $file)${NC} is found, ${red}deleting and overwriting now!${NC}"
+                rm "${HOME}/$(basename $file)"
+            else
+              echo "Linking ${magenta}$file${NC} to ${magenta}$TARGET_LOCATION/$(basename $file)${NC}"
+               ln -s "$file" "${HOME}/$(basename $file)"
+            fi
+        fi
+    done
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
