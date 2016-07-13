c3bf29f788c7778e5932b41df2f7c0cee80ddf87
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2016-07-13 12:54:19 +0100

    fixed merged install.sh

--- install.sh
+++ install.sh
@@ -12,23 +12,6 @@ export SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SOURCE_LOCATION="$SCRIPT_DIRECTORY" # Contains the files and directories I want to work with.
 
 function linkDotFiles() {
-<<<<<<< HEAD
-    # Link the files from source to the target with a dot appended to the front.
-    find $SOURCE_LOCATION/dotfiles -mindepth 1 -maxdepth 1 -name '.*'| while read file; do
-        if [[ $(basename $file) != ".git" && $(basename $file) != ".gitignore" ]]; then
-            if [[ -d ${HOME}/$(basename $file) ]]; then
-                echo "${NC}$(basename $file)${NC} is a driectory, ${red}removing a directory now ..${NC}"
-                rm -r "${HOME}/$(basename $file)"
-            elif [[ -f ${HOME}/$(basename $file) ]]
-                echo "${magenta}$(basename $file)${NC} is found, ${red}deleting and overwriting now!${NC}"
-                rm "${HOME}/$(basename $file)"
-            else
-              echo "Linking ${magenta}$file${NC} to ${magenta}$TARGET_LOCATION/$(basename $file)${NC}"
-               ln -s "$file" "${HOME}/$(basename $file)"
-            fi
-        fi
-    done
-=======
 	# Link the files from source to the target with a dot appended to the front.
 	find $SOURCE_LOCATION/dotfiles -mindepth 1 -maxdepth 1 -name '.*'| while read file; do
 		if [[ $(basename $file) != ".git" && $(basename $file) != ".gitignore" ]]; then
@@ -44,7 +27,6 @@ function linkDotFiles() {
     		fi
         fi
 	done
->>>>>>> master
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
